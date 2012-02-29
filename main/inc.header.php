4bc54d558de1f747a11c24fa41d89dab346f409d
iicmaster <iicmaster@live.com>
2012-02-29 23:42:38 +0700

    ย้าย ใบสั่งซื้อวัตถุดิบ ไปอยู่ในเมนู วัตถุดิบ

--- main/inc.header.php
+++ main/inc.header.php
@@ -9,10 +9,12 @@
 		<li><a href="index.php">หน้าแรก</a></li>
 		<li><a href="member.php">สมาชิก</a></li>
 		<li><a href="supplier.php">ผู้จัดจำหน่าย</a></li>
-		<li><a href="material_order.php">ใบสั่งซ์้อวัตถุดิบ</a></li>
 		<li>
 			<a href="material.php">วัตถุดิบ</a>
-			<ul><li><a href="material_stock.php">วัตถุดิบที่แนะนำให้ซื้อเพิ่ม</a></li></ul>
+			<ul>
+				<li><a href="material_stock.php">วัตถุดิบที่แนะนำให้ซื้อเพิ่ม</a></li>
+				<li><a href="material_order.php">ใบสั่งซ์้อวัตถุดิบ</a></li>
+			</ul>
 		</li>
 		<li><a href="product.php">สินค้า</a></li>
 		<li><a href="production.php">ผลิตสินค้า</a></li>
