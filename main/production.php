edee3020285197261523a8691ebef4860cf01d87
iicmaster <iicmaster@live.com>
2012-04-26 21:06:34 +0700

    Added some come

--- main/production.php
+++ main/production.php
@@ -1,6 +1,6 @@
-<?php 
-require_once("../include/session.php"); 
-require('../include/connect.php');
+<?php
+require_once ("../include/session.php");
+require ('../include/connect.php');
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -8,26 +8,19 @@ require('../include/connect.php');
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>ระบบจัดการผลิตและจำหน่ายสละลอยแก้ว</title>
 <style type="text/css">
-#product_stock { float: right; }
 </style>
-<?php include("inc.css.php"); ?>
+<?php include ("inc.css.php") ?>
 <script type="text/javascript" src="../js/jquery-1.5.1.min.js"></script>
 <script type="text/javascript">
-$(function(){
-	$("#id_product").change(function(){
-		
-		var id_product = $(this).val();
-		
-		$.get("production_get_quantity.php", { "id_product": id_product }, function(data){
-		   $("#quantity").html(data);
-		 });
-	});
+$(function()
+{
+
 });
 </script>
 </head>
 <body>
 <div id="container">
-	<?php include("inc.header.php"); ?>
+	<?php include ("inc.header.php") ?>
 	<div id="content">
 		<h1>คำนวนการผลิต</h1>
 		<hr />
@@ -41,38 +34,39 @@ $(function(){
 					<th scope="col">ยอดการสั่งซื้อสินค้า</th>
 					<th scope="col">รวมสินค้าที่ต้องผลิตทั้งหมด</th>
 				</tr>
+				<!-- @formatter:off -->
 				<?php 
 				$sql = 'SELECT * FROM product';
 				$query = mysql_query($sql);
-				while($data = mysql_fetch_array($query))
-				{
-					echo '<tr>
-								<td>'.$data['name'].'</td>
-								<td class="right">'.$data['total'].'</td>
-								<td class="right">'.($data['stock_max'] - $data['total']).'</td>
-								<td></td>
-								<td></td>
-							</tr>';	
-				}
+				while($data = mysql_fetch_array($query)):
 				?>
+				<tr>
+					<td><?php echo $data['name'] ?></td>
+					<td class="right"><?php echo $data['total'] ?></td>
+					<td class="right"><?php echo ($data['stock_max'] - $data['total']) ?></td>
+					<td></td>
+					<td></td>
+				</tr>
+				<?php endwhile ?>
+				<!--@formatter:on-->
 			</table>
-		<h3>วัตถุดิบที่ต้องใช้</h3>
-		<table>
-			<thead>
-				<tr>
-					<th>วัตถุดิบ</th>
-					<th>จำนวน</th>
-					<th>หน่วย</th>
-				</tr>
-			</thead>
-			<tbody>
-				<tr>
-					<td></td>
-					<td></td>
-					<td></td>
-				</tr>
-			</tbody>
-		</table>
+			<h3>วัตถุดิบที่ต้องใช้</h3>
+			<table>
+				<thead>
+					<tr>
+						<th>วัตถุดิบ</th>
+						<th>จำนวน</th>
+						<th>หน่วย</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td></td>
+						<td></td>
+						<td></td>
+					</tr>
+				</tbody>
+			</table>
 		</div>
 		<form method="get" action="production_queue_add.php">
 			<p class="center">
@@ -81,7 +75,7 @@ $(function(){
 			<?php echo $data['unit'] ?>
 		</form>
 	</div>
-	<?php include("inc.footer.php"); ?>
+	<?php include ("inc.footer.php") ?>
 </div>
 </body>
 </html>
