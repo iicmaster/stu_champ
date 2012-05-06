cb3273304953ec91e01fccb3ce5d6637ee94dd8f
iicmaster@vaio <iicmaster@live.com>
2012-05-06 15:07:20 +0700

    Updated code in production page

--- main/production.php
+++ main/production.php
@@ -8,6 +8,7 @@ require ('../include/connect.php');
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>ระบบจัดการผลิตและจำหน่ายสละลอยแก้ว</title>
 <style type="text/css">
+#product_stock h3 { margin-bottom: 10px; }
 </style>
 <?php include ("inc.css.php") ?>
 <script type="text/javascript" src="../js/jquery-1.5.1.min.js"></script>
@@ -27,44 +28,110 @@ $(function()
 		<div id="product_stock">
 			<h3>จำนวนสินค้าที่ต้องผลิต</h3>
 			<table>
-				<tr>
-					<th scope="col">สินค้า</th>
-					<th scope="col">จำนวนคงเหลือ</th>
-					<th scope="col">จำนวนที่ควรผลิตเพิ่ม</th>
-					<th scope="col">ยอดการสั่งซื้อสินค้า</th>
-					<th scope="col">รวมสินค้าที่ต้องผลิตทั้งหมด</th>
-				</tr>
-				<!-- @formatter:off -->
-				<?php 
-				$sql = 'SELECT * FROM product';
-				$query = mysql_query($sql);
-				while($data = mysql_fetch_array($query)):
-				?>
-				<tr>
-					<td><?php echo $data['name'] ?></td>
-					<td class="right"><?php echo $data['total'] ?></td>
-					<td class="right"><?php echo ($data['stock_max'] - $data['total']) ?></td>
-					<td></td>
-					<td></td>
-				</tr>
-				<?php endwhile ?>
-				<!--@formatter:on-->
+                <thead>
+    				<tr>
+    					<th scope="col">สินค้า</th>
+    					<th scope="col">จำนวนคงเหลือ</th>
+    					<th scope="col">จำนวนที่ควรผลิตเพิ่ม</th>
+    					<th scope="col">ยอดการสั่งซื้อสินค้า</th>
+    					<th scope="col">รวมสินค้าที่ต้องผลิตทั้งหมด</th>
+                        <th scope="col">หน่วย</th>
+    				</tr>
+                </thead>
+                <tbody>
+    				<!-- @formatter:off -->
+    				<?php 
+    				$total_manufactured = array();
+                    
+    				$sql = 'SELECT * FROM product';
+    				$query = mysql_query($sql);
+    				while($data = mysql_fetch_array($query)):
+                        
+                        $sql_order = 'SELECT SUM(quantity) AS order_qty 
+                                      FROM product_order_item 
+                                      WHERE id_product = '.$data['id'];
+                                      
+                        $query_order = mysql_query($sql_order);
+                        
+                        $order_qty = mysql_fetch_assoc($query_order);
+                        $order_qty = ($order_qty['order_qty'] > 0) ? $order_qty['order_qty'] : 0;
+                        $total_manufactured[$data['id']] = ($data['stock_max'] - $data['total']) + $order_qty;
+    				?>
+    				<tr>
+    					<td><?php echo $data['name'] ?></td>
+    					<td class="right"><?php echo $data['total'] ?></td>
+    					<td class="right"><?php echo ($data['stock_max'] - $data['total']) ?></td>
+                        <td class="right"><?php echo $order_qty ?></td>
+                        <td class="right"><?php echo add_comma($total_manufactured[$data['id']]) ?></td>
+    					<td><?php echo $data['unit'] ?></td>
+    				</tr>
+    				<?php endwhile ?>
+    				<!--@formatter:on-->
+				</tbody>
 			</table>
 			<h3>วัตถุดิบที่ต้องใช้</h3>
 			<table>
 				<thead>
 					<tr>
 						<th>วัตถุดิบ</th>
-						<th>จำนวน</th>
+						<th>จำนวนที่ต้องใช้</th>
+                        <th>จำนวนคงเหลือ</th>
+                        <th>จำนวนที่ต้องซื้อเพื่ม</th>
 						<th>หน่วย</th>
 					</tr>
 				</thead>
 				<tbody>
+				    <!-- @formatter:off -->
+                    <?php                    
+                    $sql = 'SELECT 
+                                id_material as id,
+                                name,
+                                total,
+                                unit
+                            
+                            FROM product_material
+                            
+                            LEFT JOIN material
+                            ON product_material.id_material = material.id
+                            
+                            GROUP BY id_material';
+                            
+                    $query = mysql_query($sql);
+                    
+                    while($material = mysql_fetch_array($query)):
+                        
+                        $required_qty = 0;
+                        $buy_qty = 0;
+                        
+                        $sql = 'SELECT id FROM product';
+                        $result = mysql_query($sql);
+                        
+                        while($product = mysql_fetch_assoc($result))
+                        {
+                            $sql = 'SELECT quantity as qty 
+                                    FROM product_material
+                                    WHERE
+                                        id_product = '.$product['id'].'
+                                        AND id_material = '.$material['id'];
+                            $result_pm = mysql_query($sql) or die(mysql_error);
+                            $data = mysql_fetch_assoc($result_pm);
+                            
+                            $required_qty += $total_manufactured[$product['id']] * $data['qty'];
+                        }
+                        
+                        $buy_qty = $required_qty - $material['total'];
+                        $buy_qty = ($buy_qty > 0) ? $buy_qty : 0;
+                        
+                    ?>
 					<tr>
-						<td></td>
-						<td></td>
-						<td></td>
+						<td><?php echo $material['name'] ?></td>
+						<td align="right"><?php echo add_comma($required_qty) ?></td>
+                        <td align="right"><?php echo add_comma($material['total']) ?></td>
+                        <td align="right"><?php echo add_comma($buy_qty) ?></td>
+						<td><?php echo $material['unit'] ?></td>
 					</tr>
+    				<?php endwhile ?>
+                    <!--@formatter:on-->
 				</tbody>
 			</table>
 		</div>
@@ -72,7 +139,6 @@ $(function()
 			<p class="center">
 				<input id="submit" name="submit" type="submit" value="จัดคิวทำงาน" />
 			</p>
-			<?php echo $data['unit'] ?>
 		</form>
 	</div>
 	<?php include ("inc.footer.php") ?>
