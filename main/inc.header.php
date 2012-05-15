51e117aabd5042edd34c4c8342916d86ef0ef420
iicmaster@vaio <iicmaster@live.com>
2012-05-15 08:30:55 +0700

    Updated menu

--- main/inc.header.php
+++ main/inc.header.php
@@ -10,17 +10,19 @@
 		<li><a href="index.php">หน้าแรก</a></li>
 		<li><a href="member.php">สมาชิก</a></li>
 		<li><a href="supplier.php">ผู้จัดจำหน่าย</a></li>
-		<li>
-			<a href="material.php">วัตถุดิบ</a>
+		<li><a href="material.php">วัตถุดิบ</a>
 			<ul>
 				<li><a href="material_stock.php">วัตถุดิบที่แนะนำให้ซื้อเพิ่ม</a></li>
 				<li><a href="material_order.php">ใบสั่งซ์้อวัตถุดิบ</a></li>
-			</ul>
-		</li>
-		<li><a href="product.php">สินค้า</a></li>
-		<li><a href="production.php">ผลิตสินค้า</a></li>
-		<li><a href="product_order.php">การสั่งซื้อจากลูกค้า</a></li>
-		
+			</ul></li>
+		<li><a href="product.php">สินค้า</a>
+			<ul>
+				<li><a href="production.php">ผลิตสินค้า</a></li>
+				<li><a href="production_log.php">บันทึกการผลิต</a></li>
+				<li><a href="product_order.php">การสั่งซื้อจากลูกค้า</a></li>
+				<li><a href="retail.php">การขายปลีก</a></li>
+			</ul></li>
+
 		<li><a href="logout.php">ออกจากระบบ</a></li>
 	</ul>
 </div>
