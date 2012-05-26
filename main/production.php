5d475b42c21a79f41b18298c84f1b4cd97c7ea82
iicmaster@vaio <iicmaster@live.com>
2012-05-26 14:43:05 +0700

    Updated product stock in production page

--- main/production.php
+++ main/production.php
@@ -34,14 +34,77 @@ $(function()
 		<h1>คำนวนการผลิตสินค้า</h1>
 		<hr />
 		<div id="product_stock">
-			<h3>จำนวนสินค้าที่ต้องผลิต</h3>
+			<h3>จำนวนสินค้าที่ควรผลิตเพิ่ม</h3>
 			<table>
                 <thead>
     				<tr>
     					<th scope="col">สินค้า</th>
     					<th scope="col">จำนวนคงเหลือ</th>
     					<th scope="col">จำนวนที่ควรผลิตเพิ่ม</th>
-    					<th scope="col">ยอดการสั่งซื้อสินค้า</th>
+                        <th scope="col">หน่วย</th>
+    				</tr>
+                </thead>
+                <tbody>
+    				<!-- @formatter:off -->
+    				<?php 
+    				$total_produced_qty = array();
+    				$total_produced_weight = array();
+					
+    				// --------------------------------------------------
+    				// Product restock
+    				// --------------------------------------------------
+    				
+    				$product_restock_list = array();
+					$total_restock = 0;
+                    
+    				$sql = 'SELECT * FROM product';
+    				$query = mysql_query($sql) or die(mysql_error());
+					
+    				while($data = mysql_fetch_array($query)):
+						
+						$sql = 'SELECT SUM(quantity) AS total 
+								FROM product_stock 
+								WHERE id_product = '.$data['id'];
+								
+						$result = mysql_query($sql) or die(mysql_error());
+						
+						$product_stock_data = mysql_fetch_assoc($result);
+						$product_stock_qty = $product_stock_data['total'];
+						
+                        $restock_qty = $data['stock_max'] - $data['total'];
+						$total_restock += $restock_qty;
+                        $product_restock_list[$data['id']] = $restock_qty;
+    				?>
+    				<tr>
+    					<td><?php echo $data['name'] ?></td>
+    					<td class="right"><?php echo add_comma($product_stock_qty) ?></td>
+    					<td class="right"><?php echo add_comma($restock_qty) ?></td>
+    					<td><?php echo $data['unit'] ?></td>
+    				</tr>
+    				<?php endwhile ?>
+    				<!--@formatter:on-->
+				</tbody>
+                <tfoot>
+                    <tr>
+                        <td class="center" colspan="2">รวมทั้งหมด</td>
+                        <td class="right"><?php echo add_comma($total_restock) ?></td>
+                        <td>หน่วย</td>
+                    </tr>
+                </tfoot>
+			</table>
+			<h3>จำนวนสินค้าที่มีการสั่งจากลูกค้า</h3>
+			<table>
+                <thead>
+    				<tr>
+    					<th scope="col"></th>
+    					<th scope="col">เลขที่ใบสั่งซื้อ</th>
+    					<?php 
+    					$query = 'SELECT name FROM product';
+    					$result = mysql_query($query);
+						while($product = mysql_fetch_assoc($result)):
+						?>
+    					<th scope="col"><?php echo $product['name'] ?></th>
+    					<?php endwhile  ?>
     					<th scope="col">รวมสินค้าที่ต้องผลิตทั้งหมด</th>
                         <th scope="col">หน่วย</th>
     				</tr>
@@ -49,10 +112,12 @@ $(function()
                 <tbody>
     				<!-- @formatter:off -->
     				<?php 
-    				$product_restock = array();
-                    $product_ordered = array();
-    				$total_produced_qty = array();
-    				$total_produced_weight = array();
+    				// --------------------------------------------------
+    				// Product order
+    				// --------------------------------------------------
+					
+                    $product_order = array();
+					$total_order = 0;
                     
     				$sql = 'SELECT * FROM product';
     				$query = mysql_query($sql);
@@ -68,12 +133,13 @@ $(function()
                         $order_qty = ($order_qty['order_qty'] > 0) ? $order_qty['order_qty'] : 0;
                         $restock_qty = $data['stock_max'] - $data['total'];
                         
-                        $product_restock[$data['id']] = $restock_qty;
-                        $product_ordered[$data['id']] = $order_qty;
+                        $product_restock_list[$data['id']] = $restock_qty;
+                        $product_order[$data['id']] = $order_qty;
                         $total_produced_qty[$data['id']] = ($data['stock_max'] - $data['total']) + $order_qty;
                         $total_produced_weight[$data['id']] = $total_produced_qty[$data['id']] * $data['weight'];
     				?>
     				<tr>
+    					<td><input type="checkbox" name="id_order[]" value="" /><?php echo $data['name'] ?></td>
     					<td><?php echo $data['name'] ?></td>
     					<td class="right"><?php echo $data['total'] ?></td>
     					<td class="right"><?php echo add_comma($restock_qty) ?></td>
@@ -86,7 +152,7 @@ $(function()
 				</tbody>
                 <tfoot>
                     <tr>
-                        <td class="center" colspan="4">รวมทั้งหมด</td>
+                        <td class="center" colspan="5">รวมทั้งหมด</td>
                         <td class="right"><?php echo add_comma(array_sum($total_produced_qty)) ?></td>
                         <td>หน่วย</td>
                     </tr>
@@ -105,7 +171,11 @@ $(function()
 				</thead>
 				<tbody>
 				    <!-- @formatter:off -->
-                    <?php                    
+                    <?php
+    				// --------------------------------------------------
+    				// Required material
+    				// --------------------------------------------------
+    				                    
                     $sql = 'SELECT 
                                 id_material as id,
                                 name,
@@ -136,6 +206,7 @@ $(function()
                                     WHERE
                                         id_product = '.$product['id'].'
                                         AND id_material = '.$material['id'];
+										
                             $result_pm = mysql_query($sql) or die(mysql_error);
                             $data = mysql_fetch_assoc($result_pm);
                             
@@ -167,49 +238,52 @@ $(function()
                     </tr>
                 </thead>
                 <tbody>
-                <?php
-                $total_worker = round(array_sum($total_produced_weight) / 15000);
-                
-                $query = 'SELECT * FROM member';
-                $result_member = mysql_query($query);
-                
-                // Create member list
-                $query = 'SELECT * FROM member LIMIT '.$total_worker;
-                $result = mysql_query($query);
-                
-                $loop = 1;
-                while($member = mysql_fetch_assoc($result)):
-                ?>
-                    <tr>
-                        <td class="right"><?php echo $loop ?></td>
-                        <td>
-                            <select id="id_member_<?php echo $member['id'] ?>" name="id_member[]">
-                            <?php 
-                            $selected = '';
-                            while($option = mysql_fetch_assoc($result_member))
-                            {
-                                $selected = ($option['id'] == $member['id']) ? 'selected="selected"' : '';
-                                echo '<option value="'.$option['id'].'" '.$selected.'>'.$option['name'].'</option>';
-                            }
-                            ?>
-                            </select>
-                        </td>
-                        <td><?php echo $member['tel'] ?></td>
-                    </tr>
-                <?php 
-                mysql_data_seek($result_member, 0);
-                $loop++;
-                endwhile 
-                ?>
+                	<?php
+    				// --------------------------------------------------
+    				// Worker
+    				// --------------------------------------------------
+	                $total_worker = round(array_sum($total_produced_weight) / 15000);
+	                
+	                $query = 'SELECT * FROM member';
+	                $result_member = mysql_query($query);
+	                
+	                // Create member list
+	                $query = 'SELECT * FROM member LIMIT '.$total_worker;
+	                $result = mysql_query($query);
+	                
+	                $loop = 1;
+	                while($member = mysql_fetch_assoc($result)):
+	                ?>
+	                    <tr>
+	                        <td class="right"><?php echo $loop ?></td>
+	                        <td>
+	                            <select id="id_member_<?php echo $member['id'] ?>" name="id_member[]">
+	                            <?php 
+	                            $selected = '';
+	                            while($option = mysql_fetch_assoc($result_member))
+	                            {
+	                                $selected = ($option['id'] == $member['id']) ? 'selected="selected"' : '';
+	                                echo '<option value="'.$option['id'].'" '.$selected.'>'.$option['name'].'</option>';
+	                            }
+	                            ?>
+	                            </select>
+	                        </td>
+	                        <td><?php echo $member['tel'] ?></td>
+	                    </tr>
+	                <?php 
+	                mysql_data_seek($result_member, 0);
+	                $loop++;
+	                endwhile 
+	                ?>
                 </tbody>
             </table>
             <h3>หมายเหตุ</h3>
             <textarea name="description" style="width:750px"></textarea>
 		</div>
 			<p class="center">
-			    <?php foreach ($product_ordered as $key => $value): ?>
+			    <?php foreach ($product_order as $key => $value): ?>
                     <input type="hidden" name="product_ordered[<?php echo $key ?>]" value="<?php echo $value ?>" />
-                    <input type="hidden" name="product_restock[<?php echo $key ?>]" value="<?php echo $product_restock[$key] ?>" />
+                    <input type="hidden" name="product_restock[<?php echo $key ?>]" value="<?php echo $product_restock_list[$key] ?>" />
                 <?php endforeach ?>
 				<input type="submit" name="submit" value="บันทึกการผลิต" />
 			</p>
